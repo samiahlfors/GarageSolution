a642b81614f043d1ffa33f22959edc0a8ebe8d09
Sami Ahlfors <hello@samiahlfors.com>
2026-05-07 15:33:34 +0200

    feat: add some basic fields to vehicles

--- Garage/Domain/Vehicle.cs
+++ Garage/Domain/Vehicle.cs
@@ -3,4 +3,8 @@ namespace Garage.Domain;
 public abstract class Vehicle
 {
     public string LicensePlate { get; set; }
+    public string Model { get; set; }
+    public string Brand { get; set; }
+    public string Color { get; set; }
+    public int NumberOfWheels { get; set; }
 }
