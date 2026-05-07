524902512f593f12c1cce3d8ecd52b2e17232f1c
Sami Ahlfors <hello@samiahlfors.com>
2026-05-07 15:13:36 +0200

    feat: initialize first garage

--- Garage/Program.cs
+++ Garage/Program.cs
@@ -1,15 +1,27 @@
-﻿namespace Garage;
+﻿using System.Runtime.CompilerServices;
+
+namespace Garage;
 
 class Program
 {
+    private static List<Domain.Garage> _garages = [];
+    
     static void Main(string[] args)
     {
+        Initialize();
+        
         var running = true;
 
         while (running)
         {
             running = ShowMenu();
         }
+    }
+
+    private static void Initialize()
+    {
+        var garage = new Domain.Garage(5);
+        _garages.Add(garage);
     }
     
     private static bool ShowMenu()
@@ -18,7 +30,7 @@ class Program
         Console.WriteLine($"-- MAIN MENU --");
 
         var menu = new Menu("Choose an option", [
-            
+            "1 - List Garages",
             "0 - Exit"
         ]);
         menu.Show();
@@ -27,6 +39,15 @@ class Program
         
         switch (choice)
         {
+            case 1:
+                foreach (var garage in _garages)
+                {
+                    Console.WriteLine(garage.Capacity.ToString());
+                }
+                
+                Menu.Pause();
+                
+                return true;
             case 0: return false;
             default: return true;
         }
