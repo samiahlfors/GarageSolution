0a7da843f98703283241b5737a4b5db3d4e7e559
Sami Ahlfors <hello@samiahlfors.com>
2026-05-08 10:15:31 +0200

    feat: add garage presentation to garage view

--- Garage/UI/Views/GarageView.cs
+++ Garage/UI/Views/GarageView.cs
@@ -2,12 +2,29 @@ using Garage.Application;
 
 namespace Garage.Views;
 
-public class GarageView(AppState state) : View
+public class GarageView(AppState state, View previousView) : View
 {
     public override string Title => "Garage";
     public override View? Render()
     {
         // Return a view of a specific garage
-        return null;
+        
+        RenderHeader();
+        
+        Console.WriteLine($"Welcome to: {state.CurrentGarage.Name}");
+        Console.WriteLine($"We have {state.CurrentGarage.Capacity} spots");
+        
+        var menu = new Menu("Menu", [
+            "0 - Back"
+        ]);
+        menu.Show();
+
+        var choice = ConsoleInput.GetInt();
+        
+        return choice switch
+        {
+            0 => previousView,
+            _ => this
+        };
     }
 }
