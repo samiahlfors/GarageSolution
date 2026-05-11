a8f64e8ca3dbae57c341897d03e43fe85b149b28
Sami Ahlfors <hello@samiahlfors.com>
2026-05-11 09:50:12 +0200

    refactor: clean up console input

--- Garage/UI/ConsoleInput.cs
+++ Garage/UI/ConsoleInput.cs
@@ -2,10 +2,11 @@ namespace Garage;
 
 public static class ConsoleInput
 {
-    public static int GetInt()
+    public static int GetInt(bool newLine = true)
     {
         while (true)
         {
+            if (newLine) Console.Write("> ");
             var input = Console.ReadLine();
 
             if (Validator.TryParseInt(input, out var result))
@@ -17,10 +18,11 @@ public static class ConsoleInput
         }
     }
 
-    public static string GetString()
+    public static string GetString(bool newLine = true)
     {
         while (true)
         {
+            if (newLine) Console.Write("> ");
             var input = Console.ReadLine();
 
             if (Validator.TryParseString(input, out var result))
