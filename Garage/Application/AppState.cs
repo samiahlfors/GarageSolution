f11faf092f9e6763071a479a5126c011d2a1ab03
Sami Ahlfors <hello@samiahlfors.com>
2026-05-11 09:49:34 +0200

    refactor: make current garage setter private

--- Garage/Application/AppState.cs
+++ Garage/Application/AppState.cs
@@ -3,15 +3,26 @@ namespace Garage.Application;
 public class AppState
 {
     public List<Domain.Garage> Garages { get; set; }
-    public Domain.Garage? CurrentGarage { get; set; }
+    public Domain.Garage? CurrentGarage { get; private set; }
 
     public AppState()
     {
         Garages = [];
     }
 
+    public void SetCurrentGarage(Domain.Garage garage)
+    {
+        CurrentGarage = garage;
+    }
+
     public void AddGarage(Domain.Garage garage)
     {
         Garages.Add(garage);
+    }
+
+    public void RemoveGarage(Domain.Garage garage)
+    {
+        CurrentGarage = null;
+        Garages.Remove(garage);
     }
 }
