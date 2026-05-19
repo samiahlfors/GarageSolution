6fca0fc94ebda9ee3602c0869f7cff14500390ec
Sami Ahlfors <hello@samiahlfors.com>
2026-05-19 10:25:51 +0200

    feat: add search view into loop

--- Garage/UI/Views/SearchView.cs
+++ Garage/UI/Views/SearchView.cs
@@ -1,53 +1,48 @@
 using Garage.Application;
 using Garage.Domain;
 
-namespace Garage.Views;
+namespace Garage.UI.Views;
 
 public class SearchView(AppState state, Navigation navigation) : View
 {
     public override string Title => "Search";
     public override void Render()
     {
-        RenderHeader();
-        
-        Console.WriteLine($"0 to exit");
-        Console.Write($"Enter Licence Plate number: ");
-        var input = ConsoleInput.GetString(false);
-
-        if (input == "0")
+        while (true)
         {
-            navigation.GoBack();
-            return;
-        }
+            RenderHeader();
         
-        var result = FindVehicle(input);
-        if (result == null)
-        {
-            Console.Clear();
-            Console.WriteLine($"Could not find vehicle");
-            
-            Menu.Pause();
-            return;
-        }
+            Console.WriteLine($"0 to exit");
+            Console.Write($"Enter Licence Plate number: ");
+            var input = ConsoleInput.GetString(false);
 
-        var (vehicle, garage) = result.Value;
-        
-        state.SetCurrentGarage(garage);
-        navigation.NavigateTo(new VehicleView(state, navigation, vehicle));
-        
-        var menu = new Menu("Menu", [
-            "0 - Go back"
-        ]);
-        menu.Show();
-
-        var choice = ConsoleInput.GetInt();
-        
-        switch (choice)
-        {
-            case 0:
+            if (input == "0")
+            {
                 navigation.GoBack();
-                break;
-            default: break;
+                return;
+            }
+
+            if (!Validator.TryParseLicencePlate(input, out var licencePlate))
+            {
+                Console.WriteLine($"Invalid format of licence plate {input}");
+                Menu.Pause();
+                continue;
+            }
+            
+            var result = FindVehicle(licencePlate);
+            if (result == null)
+            {
+                Console.Clear();
+                Console.WriteLine($"Could not find vehicle");
+            
+                Menu.Pause();
+                return;
+            }
+
+            var (vehicle, garage) = result.Value;
+        
+            state.SetCurrentGarage(garage);
+            navigation.NavigateTo(new VehicleView(state, navigation, vehicle));
         }
     }
 
