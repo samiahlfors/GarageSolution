0f4750344329741a01c7d469f02286690fe24d11
Sami Ahlfors <hello@samiahlfors.com>
2026-05-19 10:25:36 +0200

    feat: add licence plate validation

--- Garage/Validation/Validator.cs
+++ Garage/Validation/Validator.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace Garage;
 
 public static class Validator
@@ -17,6 +19,23 @@ public static class Validator
 
         result = input;
         
+        return true;
+    }
+
+    public static bool TryParseLicencePlate(string input, out string? result)
+    {
+        /*
+         * The validation is not true to a real-world scenario.
+         * It's expecting the licence plate to be in a XXX### format.
+         * There's both XXX##X and custom ones, as well as a whole world of international plates.
+         */
+        if (input.Length != 6 || !Regex.IsMatch(input.ToLower(), @"^[a-z]{3}[0-9]{3}$"))
+        {
+            result = null;
+            return false;
+        }
+
+        result = input;
         return true;
     }
 }
