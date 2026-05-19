ff0b1d0c47918f22a315a8dc39e8096b43d84070
Sami Ahlfors <hello@samiahlfors.com>
2026-05-19 10:26:24 +0200

    chore: code cleanup

--- Garage/Domain/Entities/Vehicle.cs
+++ Garage/Domain/Entities/Vehicle.cs
@@ -5,7 +5,7 @@ namespace Garage.Domain;
 
 public abstract class Vehicle
 {
-    public string LicencePlate { get; set; }
+    public string? LicencePlate { get; set; }
     public string Model { get; set; }
     public string Brand { get; set; }
     public VehicleColor Color { get; set; }
