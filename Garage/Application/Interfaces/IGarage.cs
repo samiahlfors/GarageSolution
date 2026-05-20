d06f338d4a5e0d0990f02f07602b11bbd4397514
Sami Ahlfors <hello@samiahlfors.com>
2026-05-20 08:28:06 +0200

    feat: add garage interface

---
+++ Garage/Application/Interfaces/IGarage.cs
@@ -0,0 +1,14 @@
+using Garage.Domain;
+
+namespace Garage.Application.Interfaces;
+
+public interface IGarage
+{
+    string Name { get; }
+    int Capacity { get; }
+    int OccupiedSpots { get; }
+
+    IEnumerable<Vehicle> GetVehicles();
+    Vehicle? FindVehicle(string licencePlate);
+    IEnumerable<Vehicle> Vehicles { get; }
+}
